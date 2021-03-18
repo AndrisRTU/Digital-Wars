b459e84e0d386c3aef21ac671b2c4e2a4810021d
Andris Panovs <63678784+VilksRTU@users.noreply.github.com>
2021-03-18 12:07:42 +0200

    Add files via upload

--- headers/IngameAI.h
+++ headers/IngameAI.h
@@ -13,14 +13,13 @@ private:
     uint8_t difficulty;
     uint8_t foresightCount;
     const GameScene& scene;
-    VisualGameArea& receiver;
     int16_t scoreDifferenceHeuristic(const Node& node);
     int16_t NodeSequenceHeuristic(const Node& node, uint8_t depth);
     int16_t minimax(const Node& node, uint8_t depth, bool isMax, AlphaBetaModule alphaBeta);
     using Position = std::pair<uint8_t, uint8_t>;
     void addNode(Node& node, uint8_t depth);
 public:
-    IngameAI(uint8_t difficulty, const GameScene& scene, VisualGameArea& receiver);
+    IngameAI(uint8_t difficulty, const GameScene& scene);
     void calculatePosition();
     void setDifficulty(uint8_t difficulty);
 signals:
